e0fa858faf5d1e6fca02b781b5192147f3a615a0
Mubashir Hussain <mubashrhussain41@gmail.com>
2024-11-18 11:34:56 +0500

    admin sidebar icon changes

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -1,39 +1,59 @@
-<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
-    <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link">
-            <!--begin::Brand Image--> <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span
-                class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div>
-    <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+    <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand">
+        <!--begin::Brand Link-->
+        <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow">
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">AdminLTE 4</span>
+            <!--end::Brand Text-->
+        </a>
+        <!--end::Brand Link-->
+    </div>
+    <!--end::Sidebar Brand-->
+    
+    <!--begin::Sidebar Wrapper-->
     <div class="sidebar-wrapper">
-        <nav class="mt-2"> <!--begin::Sidebar Menu-->
+        <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
             <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                <li class="nav-item"> <a href="#" class="nav-link"> <i
-                            class="nav-icon bi bi-speedometer"></i>
+
+                <!-- Dashboard Menu Item -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-house-door"></i> <!-- Updated to Bootstrap icon -->
                         <p>
                             Dashboard
                             <i class="nav-arrow bi bi-chevron-right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item"> <a href="./index.html" class="nav-link"> <i
-                                    class="nav-icon bi bi-circle"></i>
+                        <li class="nav-item">
+                            <a href="./index.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
                                 <p>Dashboard v1</p>
-                            </a> </li>
-
+                            </a>
+                        </li>
                     </ul>
                 </li>
 
+                <!-- Components Header -->
                 <li class="nav-header">COMPONENTS</li>
-                <li class="nav-item  {{ request()->is('slider') ||request()->is('slider/create') ||request()->is('slider/edit/*') ? 'active menu-open': '' }}">
+
+                <!-- Home Page Menu -->
+                <li class="nav-item {{ request()->is('slider') || request()->is('slider/create') || request()->is('slider/edit/*') ? 'active menu-open' : '' }}">
                     <a href="{{url('slider')}}" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
+                        <i class="nav-icon fas fa-home"></i> <!-- Font Awesome home icon -->
                         <p>Home Page</p>
                     </a>
                 </li>
 
-                <li class="nav-item {{ request()->is('services') ||request()->is('services/create') ||request()->is('services/edit/*') || request()->is('whatwedo') ||request()->is('whatwedo/create') ||request()->is('whatwedo/edit/*') ? 'active menu-open': '' }}">
+                <!-- About Us Menu with submenus -->
+                <li class="nav-item {{ request()->is('services') || request()->is('services/create') || request()->is('services/edit/*') || request()->is('whatwedo') || request()->is('whatwedo/create') || request()->is('whatwedo/edit/*') ? 'active menu-open' : '' }}">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
+                        <i class="nav-icon fas fa-info-circle"></i> <!-- Font Awesome info-circle icon -->
                         <p>
                             About Us
                             <i class="nav-arrow fas fa-chevron-right"></i>
@@ -41,13 +61,13 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ url('whatwedo') }}" class="nav-link {{ request()->is('whatwedo') ||request()->is('whatwedo/create') ||request()->is('whatwedo/edit/*') ? 'active' : '' }}">
+                            <a href="{{ url('whatwedo') }}" class="nav-link {{ request()->is('whatwedo') || request()->is('whatwedo/create') || request()->is('whatwedo/edit/*') ? 'active' : '' }}">
                                 <i class="nav-icon far fa-circle"></i>
                                 <p>What We Do</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('services') }}" class="nav-link {{ request()->is('services') ||request()->is('services/create') ||request()->is('services/edit/*') ? 'active' : '' }}">
+                            <a href="{{ url('services') }}" class="nav-link {{ request()->is('services') || request()->is('services/create') || request()->is('services/edit/*') ? 'active' : '' }}">
                                 <i class="nav-icon far fa-circle"></i>
                                 <p>Services</p>
                             </a>
@@ -55,46 +75,41 @@
                     </ul>
                 </li>
 
-                <li class="nav-item  {{ request()->is('portfolio') ||request()->is('portfolio/create') ||request()->is('portfolio/edit/*') ? 'active menu-open': '' }}">
+                <!-- Portfolio Menu -->
+                <li class="nav-item {{ request()->is('portfolio') || request()->is('portfolio/create') || request()->is('portfolio/edit/*') ? 'active menu-open' : '' }}">
                     <a href="{{url('portfolio')}}" class="nav-link">
-                        <i class="nav-icon fas fa-home"></i>
+                        <i class="nav-icon fas fa-briefcase"></i> <!-- Font Awesome briefcase icon -->
                         <p>Portfolio</p>
                     </a>
                 </li>
 
-                
-                    {{-- <ul class="nav nav-treeview">
+                <!-- Blogs and Fun Facts Menu -->
+                <li class="nav-item {{ request()->is('blogs') || request()->is('blogs/create') || request()->is('blogs/edit/*') || request()->is('funfacts') || request()->is('funfacts/create') || request()->is('funfacts/edit/*') ? 'active menu-open' : '' }}">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-pen"></i> <!-- Font Awesome pen icon -->
+                        <p>
+                            About Us
+                            <i class="nav-arrow fas fa-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ url('fixed-content/create') }}"
-                                class="nav-link {{ request()->is('fixed-content/create') ? 'active' : '' }}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Fixed Content</p>
+                            <a href="{{ url('blogs') }}" class="nav-link {{ request()->is('blogs') || request()->is('blogs/create') || request()->is('blogs/edit/*') ? 'active' : '' }}">
+                                <i class="nav-icon far fa-circle"></i>
+                                <p>What We Do</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('slider/create') }}"
-                                class="nav-link {{ request()->is('slider/create') || request()->is('slider/*/edit') ? 'active' : '' }}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Sliders</p>
+                            <a href="{{ url('funfacts') }}" class="nav-link {{ request()->is('funfacts') || request()->is('funfacts/create') || request()->is('funfacts/edit/*') ? 'active' : '' }}">
+                                <i class="nav-icon far fa-circle"></i>
+                                <p>Services</p>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a href="{{ url('video/create') }}"
-                                class="nav-link {{ request()->is('video/create') ? 'active' : '' }}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Video</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ url('homepage-version/create') }}"
-                                class="nav-link {{ request()->is('homepage-version/create') ? 'active' : '' }}">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Homepage Versions</p>
-                            </a>
-                        </li>
-                    </ul> --}}
-               
-            </ul> <!--end::Sidebar Menu-->
+                    </ul>
+                </li>
+
+            </ul>
         </nav>
-    </div> <!--end::Sidebar Wrapper-->
+    </div>
+    <!--end::Sidebar Wrapper-->
 </aside>
